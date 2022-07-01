e434d095b146b192d10d02495967d9e4f71fe741
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-01 17:50:06 +0500

    12

--- basic_func12.py
+++ basic_func12.py
@@ -3,10 +3,11 @@
 # Return the value of a.
 # Round the result to 2 decimal places.
 
+# from math import round
 
 def main(a):
 
-    return round(x,2)
+    return round(a,2)
 
-x=main( 45.453)
-print()
+x=main(45.453)
+print(x)
